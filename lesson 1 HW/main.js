509adb28234cc53762ecf4c87d4c281f43db79fe
RaZZaRi0 <razzario95@gmail.com>
2023-08-15 22:52:17 +0300

    add index.html add main.js I did my homework

---
+++ lesson 1 HW/main.js
@@ -0,0 +1,74 @@
+let a1 = 'hello';
+let b1 = 'owu';
+let c1 = 'com';
+let d = 'ua';
+let e = 1;
+let f = 10;
+let j = -999;
+let g = 123;
+let h = 3.14;
+let o = 2.7;
+let l = 16;
+let z = true;
+let x = false;
+
+console.log(a1);
+console.log(b1);
+console.log(c1);
+console.log(d);
+console.log(e);
+console.log(f);
+console.log(j);
+console.log(g);
+console.log(h);
+console.log(o);
+console.log(l);
+console.log(z);
+console.log(x);
+
+
+let firstName = 'Vladyslav';
+let middleName = 'Mykolayovych';
+let lastName = 'Korzachenko';
+let person = firstName + ' ' + middleName + ' ' + lastName;
+console.log(person);
+
+
+let a = 100;
+let b = '100';
+let c = true;
+
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+
+
+let nameUser = prompt('Enter your name');
+console.log(nameUser);
+let middleNameUser = prompt( 'Enter your middle name');
+console.log(middleNameUser);
+let ageUser = prompt('Enter your age');
+console.log(ageUser);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
